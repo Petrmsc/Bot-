8b8c796193e1d6a124f02aeca9c6efa392fc3245
Petrmsc <83221010+Petrmsc@users.noreply.github.com>
2021-04-27 20:03:25 +0300

    Update yandex_bot.js

--- yandex_bot.js
+++ yandex_bot.js
@@ -4,24 +4,24 @@
 // @version      0.1
 // @description  
 // @author       You
-// @match        https://yandex.ru/
+// @match        https://yandex.ru/*
 // @icon      
 // @grant        none
 // ==/UserScript==
 
 let keywords = ["как звучит флейта", "саксофон", "гобой"];
 
-let btn = document.getElementsByClassName('mini-suggest__button')[0];
+let btn = document.getElementsByClassName('button mini-suggest__button button_theme_search button_size_search i-bem button_js_inited')[0];
 let links = document.links;
 let keyword = keywords[getRandom(0,keywords.length)];
 
 
-if(btn != undefined){
-	document.getElementsByName('text')[0].value = keyword;
-	document.getElementsByClassName('mini-suggest__button')[0].click();
+if(btn !== undefined){
+	document.getElementsByClassName('input__control input__input mini-suggest__input')[0].value = keyword;
+	document.getElementsByClassName('button mini-suggest__button button_theme_search button_size_search i-bem button_js_inited')[0].click();
 }else{
 	for(let i=0; i<links.length; i++) {
-		if(links[i].href.indexOf('xn----7sbab5aqcbiddtdj1e1g.xn--p1ai')!=-1) {
+		if(links[i].href.indexOf('https://www.youtube.com/watch?v=MlviI7gyXwI')!=-1) {
 			let link = links[i];
                         link.removeAttribute('target');
 			link.click();
